1b852012cc3f3e1a722abd241da3f89cf2c93e87
Vladieboy <vlad.tikunov@gmail.com>
2019-04-04 22:37:43 -0700

    out of hell

--- src/components/login/loginPage.jsx
+++ src/components/login/loginPage.jsx
@@ -1,79 +1,44 @@
-<<<<<<< HEAD
-import React, {
-    useState,
-    useEffect
-} from "react";
-import { AvForm, AvField } from 'availity-reactstrap-validation';
-import { Button } from 'reactstrap';
-import * as userService from "../../services/userService"
-import {saveState} from '../../localStorage'
-=======
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import * as userService from "../../services/userService";
+import { saveState } from "../../localStorage";
 import { AvForm, AvField } from "availity-reactstrap-validation";
 import { Button, Container, Row, Col } from "reactstrap";
->>>>>>> master
 
 const Login = props => {
-  function handleValidSubmit(event, values) {
-    this.setState({ email: values.email });
-  }
+  const [userData, setUserData] = useState([{ email: "", password: "" }]);
 
-<<<<<<< HEAD
-const [userData, setUserData] = useState([{email: '', password: ''}])
-
-
-const handleValidSubmit = (event, values) => {
-    console.log("event", event, "values", values)
-    setUserData(values)
-  }
+  const handleValidSubmit = (event, values) => {
+    console.log("event", event, "values", values);
+    setUserData(values);
+  };
 
   const handleInvalidSubmit = (event, errors, values) => {
-    console.log(errors)
-  }
+    console.log(errors);
+  };
 
   const handleLogin = () => {
     var details = {
-        grant_type: "password",
-        username: userData.email,
-        password: userData.password
-      };
-  
-      var formBody = [];
-      for (var property in details) {
-        var encodedKey = encodeURIComponent(property);
-        var encodedValue = encodeURIComponent(details[property]);
-        formBody.push(encodedKey + "=" + encodedValue);
-      }
-      formBody = formBody.join("&");
-      userService
-        .login(formBody).then(saveState(details)).catch()
-  }
+      grant_type: "password",
+      username: userData.email,
+      password: userData.password
+    };
 
-  useEffect(()=> {
-    handleLogin()
-  })
+    var formBody = [];
+    for (var property in details) {
+      var encodedKey = encodeURIComponent(property);
+      var encodedValue = encodeURIComponent(details[property]);
+      formBody.push(encodedKey + "=" + encodedValue);
+    }
+    formBody = formBody.join("&");
+    userService
+      .login(formBody)
+      .then(saveState(details))
+      .catch();
+  };
 
-    return (
-        <div>
-            <h3>Login</h3>
-        <AvForm onValidSubmit={handleValidSubmit} onInvalidSubmit={handleInvalidSubmit}>
-           <AvField name="email" label="Email Address" type="email" value={userData.email} required />
-           <AvField name="password" label="Password" type="password" value={userData.password} required />
-          <Button color="primary">Submit</Button>
-        </AvForm>
-
-<button>Register</button>
-   
-       
-      </div>
-    )
-}
-
-export default Login;
-=======
-  function handleInvalidSubmit(event, errors, values) {
-    this.setState({ email: values.email, error: true });
-  }
+  useEffect(() => {
+    handleLogin();
+  });
 
   return (
     <Container>
@@ -100,4 +65,3 @@ export default Login;
 };
 
 export default Login;
->>>>>>> master
